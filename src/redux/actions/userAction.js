d23a66e116243385ada6786b66fe4cba45b240b9
Portia-Nelly-Mashaba <pnpnelly@gmail.com>
2024-11-06 09:24:23 +0200

    added users to database

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -4,12 +4,15 @@ export const registerNewUser=()=>dispatch=>{
     dispatch({type: 'USER_REGISTER_REQUEST'})
 
     axios
-    .post('/regiter')
+    .post('http://localhost:5000/register')
     .then(res => {
         dispatch({type: 'USER_REGISTER_SUCCESS'})
+        console.log(res);
+        
     })
     .catch(err => {
         dispatch({type: 'USER_REGISTER_FAILED'})
+        console.log(err);
     })
 
 
